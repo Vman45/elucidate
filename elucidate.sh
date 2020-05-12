921e2e5cc4a4498dd0031273dd27d815b7b51601
Philippe Jean Guillaumie <similar@orange.fr>
2020-05-12 09:14:45 +0200

    Refactor do_tests()

--- elucidate.sh
+++ elucidate.sh
@@ -495,9 +495,6 @@ do_tests() {
     exit 1
   fi
 
-  which git &>/dev/null || sudo apt install --yes git
-  echo
-
   # Users of VirtualBox: Comment out the following lines if you get unexpected network errors.
   git ls-remote https://git.enlightenment.org/core/efl.git HEAD &>/dev/null
   if [ $? -ne 0 ]; then
