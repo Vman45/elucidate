f2365df0329b1dd319bf7cde840d0c7aee21518e
Philippe Jean Guillaumie <similar@orange.fr>
2020-05-20 19:16:44 +0200

    Minor correction

--- elucidate.sh
+++ elucidate.sh
@@ -530,7 +530,7 @@ do_bsh_alias() {
     export LDFLAGS=-L/usr/local/lib
     export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
 
-    # Parallel build: It's your call...
+    # Parallel build? It's your call...
     #export MAKE="make -j$(($(nproc) * 2))"
 
     # This script adds the ~/.local/bin directory to your PATH environment variable if required.
